f99a877612ee1648ceca6053b8e208b5fba858d9
Ptolemy Henson <ptolemy2002@gmail.com>
2023-03-05 15:00:15 -0500

    Made an effort to override the default behavior of scrolling to sections when targeting.

--- javascript/default_section.js
+++ javascript/default_section.js
@@ -1,5 +1,14 @@
 const DEFAULT_SECTION = "video-section";
 
+function scrollToTop() {
+    window.scroll(0, 0);
+}
+
 if (document.location.hash == "" || document.location.hash == "#") {
     document.location.hash = "#" + DEFAULT_SECTION;
 }
+
+// Every time the hash changes, scroll to the top of the page
+window.addEventListener("hashchange", scrollToTop, false);
+// When the page loads, scroll to the top of the page
+window.addEventListener("load", scrollToTop, false);
