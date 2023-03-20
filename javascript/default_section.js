b6edb6904499c947fd62181ca57a9c2fb0f75d4b
Ptolemy Henson <ptolemy2002@gmail.com>
2023-03-20 14:18:11 -0400

    Fix with the default section script

--- javascript/default_section.js
+++ javascript/default_section.js
@@ -4,11 +4,21 @@ function scrollToTop() {
     window.scroll(0, 0);
 }
 
-if (document.location.hash == "" || document.location.hash == "#") {
-    document.location.hash = "#" + DEFAULT_SECTION;
+function loadDefaultSection() {
+    if (document.location.hash !== "#nav") {
+        if (document.location.hash == "" || document.location.hash == "#") {
+            document.location.hash = "#" + DEFAULT_SECTION;
+        }
+    }
 }
 
-// Every time the hash changes, scroll to the top of the page
-window.addEventListener("hashchange", scrollToTop, false);
+loadDefaultSection();
+
+// Every time the hash changes, scroll to the top of the page and load the default section
+window.addEventListener("hashchange", () => {
+    loadDefaultSection();
+    scrollToTop();
+}, false);
+
 // When the page loads, scroll to the top of the page
 window.addEventListener("load", scrollToTop, false);
